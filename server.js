ff6da0dcff6ebe19388bdae596d743e7ebe3394e
Victor Hugo <victorhugorch@gmail.com>
2017-04-13 21:16:48 -0300

    Make a request to XplainShell

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const telegramBot = require('node-telegram-bot-api'),
       dotenv      = require('dotenv').config(),
+      request     = require('request'),
+      cheerio     = require('cheerio');
       token       = process.env.TELEGRAM_API,
       bot         = new telegramBot(token, { polling: true });
 
-bot.on('message', function(msg) {
-  let chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Im working!');
+bot.on('message', (msg) => {
+  let userID = msg.chat.id;
+  bot.sendMessage(userID, 'I\'m working!');
 });
+
+request('https://explainshell.com/')
+  .on('data', (data) => {
+    let $ = cheerio.load(data);
+    //Colocar a mensagem do user dentro de val.
+    let input = $('#explain').val('blablabla');
+    //console.log(input.toString());
+  })
+  .on('end', () => {
+    //
+  });
