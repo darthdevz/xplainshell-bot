235ee6f9fd20ace5613645cf07df7bc3acb8abf7
Victor Hugo <victorhugorch@gmail.com>
2017-04-14 21:18:54 -0300

    wrinting more beaut

--- xplainshell.js
+++ xplainshell.js
@@ -6,16 +6,16 @@ const telegramBot = require('node-telegram-bot-api'),
       bot         = new telegramBot(token, { polling: true });
 
 bot.on('message', (msg) => {
-  let userID      = msg.chat.id;
-  let messageUser = msg.text;
-  let url         = 'https://explainshell.com/explain?cmd='+ messageUser;
+  let userID      = msg.chat.id,
+      messageUser = msg.text,
+     url         = 'https://explainshell.com/explain?cmd='+ messageUser;
 
   request.get(url, (err, res) => {
     if (err) throw err;
 
-    let $            = cheerio.load(res.text);
-    let answer       = $('.help-box').text();
-    let errorMessage = "Sorry, this command is invalid or unknown. Try another command."
+    let $            = cheerio.load(res.text),
+        answer       = $('.help-box').text(),
+        errorMessage = "Sorry, this command is invalid or unknown. Try another command.";
 
     bot.sendMessage(userID, answer).catch((error) => {
       bot.sendMessage(userID, errorMessage);
